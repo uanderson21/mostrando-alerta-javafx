82b9001239fd4e4aab39b9402cf41c34cee5b97e
Uanderson Santana <udsantos@indracompany.com>
2022-05-05 20:23:30 -0300

    Exemplo de Alerta utilizando javaFX

---
+++ src/gui/ViewController.java
@@ -0,0 +1,18 @@
+package gui;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
+
+public class ViewController {
+	
+	@FXML
+	private Button btnTest;
+	
+	@FXML
+	public void onBtTestAction() {
+		Alerts.showAlert("Alert title", "Alert header", "Hello", AlertType.INFORMATION);
+	}
+
+}
